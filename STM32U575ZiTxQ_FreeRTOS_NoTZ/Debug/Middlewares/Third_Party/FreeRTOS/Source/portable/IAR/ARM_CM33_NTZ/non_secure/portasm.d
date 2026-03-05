a778083f6a29e5c8d19edf39b22b7d187585a30c
z3lthyy <davide.lobianco@gmail.com>
2026-03-05 18:32:55 +0100

    created board project with CubeMX

---
+++ STM32U575ZiTxQ_FreeRTOS_NoTZ/Debug/Middlewares/Third_Party/FreeRTOS/Source/portable/IAR/ARM_CM33_NTZ/non_secure/portasm.d
@@ -0,0 +1,6 @@
+Middlewares/Third_Party/FreeRTOS/Source/portable/IAR/ARM_CM33_NTZ/non_secure/portasm.o: \
+ ../Middlewares/Third_Party/FreeRTOS/Source/portable/IAR/ARM_CM33_NTZ/non_secure/portasm.s \
+ ../Core/Inc/FreeRTOSConfig.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_syscall_numbers.h
+../Core/Inc/FreeRTOSConfig.h:
+../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_syscall_numbers.h:
