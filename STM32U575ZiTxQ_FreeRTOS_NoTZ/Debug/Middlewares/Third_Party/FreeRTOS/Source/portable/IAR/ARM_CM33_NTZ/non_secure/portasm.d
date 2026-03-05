8fb0de52445da3e94e219f4bf07b99f2f3d3d038
z3lthyy <davide.lobianco@gmail.com>
2026-03-05 18:58:48 +0100

    Regenerated code from CubeMX due to compilation errors, new code compiles correctly

--- STM32U575ZiTxQ_FreeRTOS_NoTZ/Debug/Middlewares/Third_Party/FreeRTOS/Source/portable/IAR/ARM_CM33_NTZ/non_secure/portasm.d
+++
@@ -1,6 +0,0 @@
-Middlewares/Third_Party/FreeRTOS/Source/portable/IAR/ARM_CM33_NTZ/non_secure/portasm.o: \
- ../Middlewares/Third_Party/FreeRTOS/Source/portable/IAR/ARM_CM33_NTZ/non_secure/portasm.s \
- ../Core/Inc/FreeRTOSConfig.h \
- ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_syscall_numbers.h
-../Core/Inc/FreeRTOSConfig.h:
-../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_syscall_numbers.h:
--- STM32U575ZiTxQ_FreeRTOS_NoTZ/Debug/sources.mk
+++ STM32U575ZiTxQ_FreeRTOS_NoTZ/Debug/sources.mk
@@ -29,6 +29,5 @@ Drivers/STM32U5xx_HAL_Driver/Src \
 Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2 \
 Middlewares/Third_Party/FreeRTOS/Source \
 Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM33_NTZ/non_secure \
-Middlewares/Third_Party/FreeRTOS/Source/portable/IAR/ARM_CM33_NTZ/non_secure \
 Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang \
 
